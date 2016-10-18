aaaec44b3528e03ccfb7bfb788d40715ca40326c
Lenin Medeiros <lenin@MB-033100.clients.vu.nl>
2016-10-18 15:46:39 +0200

    new versions for the keywords and the templates

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,5 @@
 RELATIONSHIP = ["relationship", "girlfriend", "boyfriend", "wife",
-                "husband", "marriage", "divorce", "broke up",
+                "husband", "marriage", "divorce", "divorced", "broke up",
                 "my ex", "brokes up", "break up", "crush",
                 "fiancé", "broke my heart", "romance", "state",
                 "anaclisis", "love affair", "affair", "sexual relationship",
@@ -12,43 +12,51 @@ RELATIONSHIP = ["relationship", "girlfriend", "boyfriend", "wife",
                 "ux.", "widow", "widower", "homemaker", "first lady", "mayoness",
                 "signora", "battle-axe", "crown princess", "sheika", "love",
                 "lover", "beau", "swain", "spousal", "wedlock", "matrimony",
-                "nuptials", "honeymoon"]
+                "nuptials", "honeymoon", "cheated on", "cheat on",
+                "someone else", "passion"]
 
 WORK = ["work", "working", "boss", "work load", "job", "task",
         "project", "company", "production", "nightwork", "jobs", "duty",
-        "mission", "wash", "substituting", "ironing", "operation", "labour",
-        "shining", "tending", "social service", "heavy lifting", "service",
+        "duties", "mission", "wash", "substituting", "ironing", "operation",
+        "labour", "shining", "tending", "social service", "heavy lifting",
         "loose end", "labor", "polishing", "housework", "investigation",
         "timework", "make-work", "lavation", "welfare work", "paperwork",
         "toil", "attention", "spadework", "unfinished business", "action",
         "investigating", "washing", "subbing", "care", "missionary work",
         "logging", "housekeeping", "housewifery", "undertaking", "coursework",
         "activity", "busywork", "aid", "procedure", "old man", "employer",
-        "hirer", "guvnor", "foreman", "gaffer", "supervisor", "baas",
+        "hirer", "guvnor", "foreman", "gaffer", "supervisor", "superior", "baas",
         "assistant foreman", "chief", "honcho", "ganger", "straw boss", "leader",
         "executive", "administrator", "general manager", "manager", "colleague",
-        "employee", "fired", "hired", "hiring", "firing", "deadline", "business"]
+        "employee", "fired", "hired", "hiring", "firing", "deadline",
+        "business", "service"]
 
-DEATH = ["death", "died", "passed away", "loss", "passed out", "decease",
-         "expiry", "passing away", "passing out", "dying"]
+DEATH = ["death", "died", "dead", "passed away", "pass away", "loss",
+         "passed out", "decease",
+         "expiry", "passing away", "passing out", "dying", "bury",
+         "buried", "funeral", "killed",
+         "deceased", "no more", "rest in piece"]
 
 FINANCIAL = ["financial", "salary", "paycheck", "money", "debt", "debts",
              "business", "fiscal", "monetary", "fund", "currency", "cash",
              "pound", "pounds", "dollar", "dollars", "bank", "investment",
              "investments", "bill", "bills", "euro", "euros", "$", "€", "£",
              "wealth", "bundle", "pile", "sterling", "currency", "atm", "account",
-             "insurance", "price", "rent"]
+             "insurance", "price", "rent", "cent", "penny", "pound", "broke",
+             "lend", "borrow", "loan"]
 
-HEALTH = ["disease", "illness", "ill", "surgery", "tumor",
-           "cancer", "hospital", "doctor", "aids", "hiv", "virus",
-           "treatment", "pain", "headache", "injury", "injuried", "health",
-           "doctors", "healthy", "unhealthy", "sick", "nauseated", "bacteria",
-           "allergy", "cough", "cold", "blood", "urine", "smoking", "smoke"]
+HEALTH = ["disease", "illness", "ill", "surgery", "tumor", "diagnose",
+           "cancer", "hospital", "doctor", "aids", "hiv", "virus", "infection",
+           "treatment", "pain", "headache", "injury", "injuried", "injured",
+           "doctors", "healthy", "unhealthy", "sick", "sickness", "nauseated",
+           "allergy", "cough", "cold", "blood", "urine", "smoking", "smoke",
+           "suffer", "suffering", "diagnosis", "infected", "health", "bacteria",
+           "condition",]
 
-EDUCATIONAL = ["exams", "exam", "school", "college", "course",
+EDUCATIONAL = ["exams", "exam", "school", "college", "course", "learn"
          "university", "test", "grade", "teacher", "examination", "question sheet",
          "phd", "my master", "thesis", "dissertation", "paper", "article", "journal",
-         "conference", "experiment", "data analysis"]
+         "conference", "experiment", "data analysis", "education"]
 
 DICTIONARIES = [RELATIONSHIP, WORK, DEATH, FINANCIAL, HEALTH, EDUCATIONAL]
 
--- templates.py
+++ templates.py
@@ -3,32 +3,34 @@ ges_msg1 = (
     "<user>, I'm so sorry to hear this from you and such a"
     " <situation>situation can be tough, indeed. But I'm sure you are"
     " surrounded by at least a few"
-    " people who really cares about you and they could do whatever they can to"
+    " people who really care about you and they could do whatever they can to"
     " see you better. Talk to them and share your feelings. I'm sure it will"
-    " worth since they really love you so they'll support you as you need."
+    " be worth it since they really love you so they'll support you the way"
+    " you need."
 )
 
 ges_msg2 = (
     "Oh, <user>, I'm so sorry to hear this from you. I bet there are some"
     " people who cares about you and they can provide support in order to help"
-    " you in dealing with this <situation>situation. I bet they feel sorry as"
-    " well. Try to talk to them and for sure you'll be fine."
+    " you in dealing with this <situation>situation. I bet they feel sorry for"
+    " you as well. Try to talk to them and for sure you'll be fine."
 )
 
 ges_msg3 = (
-    "Hey, <user>, I'm so sorry to hear from you this <situation>situation."
+    "Hey, <user>, I'm so sorry to hear this <situation>situation from you."
     " I can imagine how tough it can be. But I bet there are some people"
-    " who cares about you and they can provide you the best support you need."
-    " Take care and I get well soon!"
+    " who care about you and they can provide you the best support you need."
+    " Take care!"
 )
 
 ges_msg4 = (
     "Oh, <user>, I hope you are OK now and hopefully this"
-    " <situation>situation won't be that relevant for you soon. Take care!"
+    " <situation>situation won't be that relevant for you soon. You are a"
+    " good person. Take care!"
 )
 
 ges_msg5 = (
-    "Hey, <user>, I can imagine how annoying can be to deal with this"
+    "Hey, <user>, I can imagine how difficult it can be to deal with this"
     " <situation>situation. I'm so sorry to hear this from you. I hope you"
     " can get over it as soon as possible."
 )
@@ -41,19 +43,19 @@ ges_msg6 = (
 
 ges_msg7 = (
     "Hey, <user>, I can imagine how difficult could be to deal with this"
-    " <situation>situation. I'm sorry for this. I hope you can get over it"
-    " soon, OK?"
+    " <situation>situation. I'm so sorry for this. I hope you can get over it"
+    " soon."
 )
 
 ges_death1 = (
-    "<user>, I'm so sorry for your lost. Actually, I'm pretty sure that all"
-    " the good people who really cares about you are feeling this lost as"
+    "<user>, I'm so sorry for your loss. Actually, I'm pretty sure that all"
+    " the good people who really care about you are feeling this loss as"
     " well. Stay close to them so they can give you the support you need in"
     " this hard moment of life."
 )
 
 ges_death2 = (
-    "<user>, I'm really sorry for your lost. I hope you can feel better as"
+    "<user>, I'm really sorry for your loss. I hope you can feel better as"
     " soon as possible. For sure you are surrounded by amazing people that"
     " can provide you all the support you need. Be with them and take care!"
 )
@@ -65,28 +67,28 @@ GES_DEATH = [ges_death1, ges_death2]
 # cognitive change messages
 cc_msg1 = (
     "Oh, <user>, that's not a good thing indeed, but try to think"
-    " positively! I bet it could be worse and everyone has to face this"
+    " positively! I bet it could be worse and many people face this"
     " kind of <situation>situation. Try to focus on the things you have"
     " learned from all of this. Do this exercise: what did you learn from"
-    " this situation? Answer this question to yourself and then you can"
-    " realize to what extent you have anything to do in order to avoid"
+    " this situation? Answer this question for yourself and then you can"
+    " realize to what extent you can do anything in order to avoid"
     " this in the future."
 )
 
 cc_msg2 = (
     "Always look on the bright side of life,"
     " <user>! It could be worse and problems like this"
-    " <situation>situation usually happen with everybody. However,"
+    " <situation>situation happen to many people. However,"
     " some people can"
     " extract good lessons from these situations. You should do the same."
     " What can you learn from all of this? Think about this situation you"
-    " told me and try to answer this question to yourself. I bet you'll"
+    " told me about and try to answer this question to yourself. I bet you'll"
     " be fine and you can avoid or at least deal better with this same"
     " kind of situation in the future."
 )
 
 cc_msg3 = (
-    "<user>, a <situation>situation like the one you told me seems to be"
+    "<user>, a <situation>situation like this seems to be"
     " tough, indeed. My advice is: try to think about all the things you can"
     " extract from this that you can use in the future in order to avoid"
     " facing such a situation again. At least you are learning something from"
@@ -94,7 +96,7 @@ cc_msg3 = (
 )
 
 cc_msg4 = (
-    "You know what, <user>? Could be helpful to look at the bright side, so"
+    "You know what, <user>? It could be helpful to look at the bright side, so"
     " why don't you think about the lessons you learned from this"
     " <situation>situation? I bet you'll feel better and this way you can"
     " avoid facing this again in the future."
@@ -103,18 +105,18 @@ cc_msg4 = (
 cc_msg5 = (
     "Oh, <user>, don't worry too much… at least you are learning important"
     " lessons from this <situation>situation, right? Therefore, see this as a"
-    " learning process and you'll feel better. Besides that, you can easily"
+    " learning process and you'll feel better. Besides that, you now"
     " know how to proceed in order to avoid this in the future."
 )
 
 cc_msg6 = (
     "<user>, think positively! I bet you are learning important things from"
     " this <situation>situation. You'll carry such a knowledge with yourself"
-    " forever so you can easily avoid facing it in the future."
+    " forever so you can better deal with similar cases in the future."
 )
 
 cc_msg7 = (
-    "Oh, <user>, this <situation>situation seems to be tough. I think you"
+    "Oh, <user>, this <situation>situation seems to be tough. But I think you"
     " should look at the bright side. So think about the things you are"
     " learning from this. I believe this way you'll feel better and you can"
     " avoid facing the same situation in the future."
@@ -124,16 +126,16 @@ cc_death1 = (
     "<user>, death is part of life. The most important thing, then, is how we"
     " live and the good social connections we make during our journey. I'm"
     " sure you have amazing memories to remember. This way you can carry"
-    " forever with you anyone you want! I'm sure you be fine one day,"
+    " anyone you want with you forever! I'm sure you'll be fine one day,"
     " so let time works."
 )
 
 cc_death2 = (
-    "<user>, it's always hard to lose someone we care… it happens with"
+    "<user>, it's always hard to lose someone we care about… it happens to"
     " everyone since death is a natural aspect of life. Try to think about"
     " all the good things this person did and showed to you during lifetime."
     " You can use all the good lessons and memories in your favor forever"
-    " and this way everybody we care could be somehow immortal."
+    " and this way everybody we care about could in a sense be immortal."
 )
 
 CC = [cc_msg1, cc_msg2, cc_msg3, cc_msg4, cc_msg5, cc_msg6, cc_msg7]
@@ -148,7 +150,7 @@ ad_msg1 = (
 )
 
 ad_msg2 = (
-    "Hey, <user>, how about you thinking in anything else? I believe it's"
+    "Hey, <user>, how about you thinking about anything else? I believe it's"
     " better to keep this <situation>situation out of your mind. For sure you"
     " have other stuff to occupy your head. You can even try to go out for a"
     " walk while listening some music, etc. when it's really hard to avoid"
@@ -156,7 +158,7 @@ ad_msg2 = (
 )
 
 ad_msg3 = (
-    "<user>, don't think about this <situation>situation. Let your mind"
+    "<user>, don't think about this <situation>situation. Let your mind be"
     " focused on other things that can make you happy. You'll be fine this way!"
 )
 
@@ -167,9 +169,9 @@ ad_msg4 = (
 )
 
 ad_msg5 = (
-    "Oh, <user>, it's such a hard <situation>situation to face. You can just"
-    " stop thinking about it. Try to let your mind focused on other stuff."
-    " This way I bet you'll feel better."
+    "Oh, <user>, it's such a hard <situation>situation that you are facing."
+    " But you should just stop thinking about it. Try to let your mind be"
+    " focused on other stuff. This way I bet you'll feel better."
 )
 
 ad_msg6 = (
@@ -193,22 +195,22 @@ AD_DEATH = [ad_death1]
 
 # situation selection messages
 ss_msg1 = (
-    "<user>, I think you can avoid this <situation>situation. For sure you"
-    " have other important tasks or things to deal with, right? So stay"
-    " focused on them, do what you have to do and let it go. This way I think"
-    " you're gonna feel better."
+    "<user>, I think you can avoid being confronted with this"
+    " <situation>situation too much. For sure you have other important tasks or"
+    " things to deal with, right? So stay focused on them, do what you have"
+    " to do and let it go. This way I thinkyou're gonna feel better."
 )
 
 ss_msg2 = (
     "Oh, <user>, this <situation>situation you told can be tough, indeed."
-    " So that's why I think you must avoid it when it's possible. Do you think"
-    " you can? If so, do the other things that for sure you have to do and try"
-    " to avoid facing this situation. Let it go!"
+    " So that's why I think you must avoid being confronted with it, as far as"
+    " it's possible. Do you think you can? If so, do the other things that for"
+    " sure you have to do and try to avoid facing this situation. Let it go!"
 )
 
 ss_msg3 = (
-    "Hey, <user>, it's such a tough <situation>situation this one you just"
-    " told me. Sometimes it's not worth facing situations like that because"
+    "Hey, <user>, what you just told me really is a tough <situation>situation"
+    " Sometimes it's not worth facing situations like that because"
     " you can't do anything about it. Don't you think that's the case now?"
     " Think about it because this way I believe you'll feel better. Try to"
     " focus on other things that make you happy."
@@ -218,27 +220,28 @@ ss_msg4 = (
     "<user>, for some cases like this <situation>situation you told me,"
     " it's better to avoid facing it. For sure you have other things or tasks"
     " to deal with, right? So try to occupy your head with them and let"
-    " it go... maybe the best thing to do now is to not go for it."
+    " it go... maybe the best thing to do now is to make sure you will not face"
+    " the situation anymore"
 )
 
 ss_msg5 = (
     "Oh, <user>, this <situation>situation seems to be complicated to deal"
-    " with, indeed. But maybe it's better to avoid it. Sometimes you just"
-    " can't do anything about a given issue. Is it the case now? It might be"
-    " helpful to act like this… think about it."
+    " with, indeed. But maybe it's better to avoid being confronted with it too"
+    " often. Sometimes you just can't do anything about a given issue. Is it"
+    " the case now? It might be helpful to act like this… think about it."
 )
 
 ss_msg6 = (
     "Hey, <user>, this is such a tough <situation>situation. But to be quite"
     " honest maybe you just can't do anything about it. If that's the case so"
     " just let it go… do the other stuff you have to do and try to avoid"
-    " this situation. Think about it."
+    " being confronted woth this situation."
 )
 
 ss_msg7 = (
     "Oh, <user>, I think you can avoid this <situation>situation. What do you"
     " think? If so, do this. Try to do the other stuff you have to do instead"
-    " of facing this situation. Maybe it'll help you to be better."
+    " of facing this situation. Maybe it'll help you to feel better."
 )
 
 ss_death1 = ges_msg1
@@ -256,11 +259,11 @@ sm_msg1 = (
 )
 
 sm_msg2 = (
-    "Oh, <user>, I see that what you told isn't a good <situation>situation,"
-    " for sure, but maybe you can realize that there are things to do in order"
-    " to change it. Take your time, calm down and try to come up with a list"
-    " of things you could do to improve this situation in order to feel better"
-    " (and I bet you will)."
+    "Oh, <user>, I see that what you told me isn't a good <situation>situation,"
+    " for sure, but maybe you can realize that there are things you can do in"
+    " order to change it. Take your time, calm down and try to come up with a"
+    " list of things you could do to improve this situation in order to feel"
+    " better (and I bet you will)."
 )
 
 sm_msg3 = (
@@ -273,7 +276,7 @@ sm_msg3 = (
 sm_msg4 = (
     "Hey, <user>, this is a bad <situation>situation, for sure. But calm down."
     " What if there is anything that you could do right now in order to change"
-    " this? Have you thought about about it? Do this know and I bet you'll"
+    " this? Have you thought about about it? Do this now and I bet you'll"
     " figure out how to proceed and then you'll feel better."
 )
 
@@ -287,33 +290,33 @@ sm_msg5 = (
 
 sm_msg6 = (
     "Hey, <user>, it seems to be a difficult <situation>situation. But in most"
-    " part of the cases there is at least one thing we could do in order to"
+    " cases there is at least one thing we could do in order to"
     " improve a given situation. Try to think how you could change this"
-    " situation right now and then you'll feel better."
+    " situation right now and you'll feel better."
 )
 
 sm_msg7 = (
     "<user>, try to calm down. Even though it seems to be a hard"
-    " <situatoin>situation, I bet there is at least one thing you could do"
-    " to improve this situation right now. Try to do this and then you'll"
+    " <situation>situation, I bet there is at least one thing you could do"
+    " to improve this situation right now. Try to do this and you'll"
     " feel better."
 )
 
 sm_death1 = (
-    "<user>, it is hard and it happens with everyone. This is probably the"
-    " only thing in life we can't do anything about. I would say that for now"
-    " you need to make an effort in order to feel better So live your life"
-    " using all the good things you learned from the important people that"
-    " already have passed away. Let it go and one day you'll accept it. Maybe"
-    " you need a few days off and then you should focus on the things you"
-    " have to do."
+    "<user>, it is indeed very hard and it happens to everyone. This is"
+    " probably the only thing in life we can't do anything about. I would say"
+    " that for now you need to make an effort in order to feel better So"
+    " live your life using all the good things you learned from the important"
+    " people that already have passed away. Let it go and one day you'll accept"
+    " it. Maybe you need a few days off and then you should focus on the things"
+    " you have to do."
 )
 
 sm_death2 = (
-    "Oh, <user>, it's always complicated to lose someone we care. It's normal"
-    " to feel such a pain in your heart, but life goes on. Try to live your"
-    " life because I bet if someone likes you so he or she would be very"
-    " happy in seeing (even if from another dimension, let's say) you dealing"
+    "Oh, <user>, it's always difficult to lose someone we care about. It's"
+    " normal to feel such a pain in your heart, but life goes on. Try to live"
+    " your life because I bet the persons you care about would be very"
+    " happy (even if from another dimension, let's say) in seeing you dealing"
     " well with your stuff. So let it go and live your life. You'll be fine"
     " one day."
 )
